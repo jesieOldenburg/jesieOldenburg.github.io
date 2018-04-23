4c48e5b50b95125b17d17e676f51cf8716227360
Jesie Oldenburg <kitglo46@gmail.com>
2018-04-23 15:22:31 -0500

    commiting changes before deploy

--- dist/app.js
+++ dist/app.js
@@ -18,9 +18,9 @@ articleCard = `
                     <div class="col-lg-10 col-xl-8 offset-lg-1 offset-xl-2">
                         <div class="intro">
                             
-                            <h2 class="text-center">${this.title}</h2> 
+                            <h2 class="text-center"></h2> 
 
-                            <p class="text-center"><span class="by">by</span> <a href="#">${this.author}</a>
+                            <p class="text-center"><span class="by">by</span> <a href="#"></a>
                                 <span class="date">Sept 8th, 2016 </span>
                             </p>
                         </div>

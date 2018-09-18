7e64e4ddbe9dc55acd07b3fd1fe3aed75aa4a9aa
Jesie Oldenburg <kitglo46@gmail.com>
2018-09-17 20:38:03 -0500

    continuing responsive styling and content addition to site

---
+++ v3/js/lineDrawing.js
@@ -0,0 +1,17 @@
+'use strict';
+console.log("line drawing is here");
+
+// let $ = require('jquery');
+var path = anime.path('#lineDrawing path');
+
+var lineDrawing = anime({
+  targets: '#lineDrawing .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});
+
+console.log("whats anime?", anime);
--- v3/js/main.js
+++ v3/js/main.js
@@ -3,5 +3,5 @@ console.log('mate is here');
 
 let animate = require('./mate.js');
 let $ = require('jquery');
+let Typed = require('typed.js');
 var el = document.querySelector('#moving-card .moving');
-

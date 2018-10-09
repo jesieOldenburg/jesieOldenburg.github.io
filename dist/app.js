b6e27eafdcd100b36e63b1fbac3a3358575e42a0
Jesie Oldenburg <kitglo46@gmail.com>
2018-10-08 19:03:39 -0500

    added coming soon to wyfy case study; fixed arrow icons at project links on landing page for dp racing and wyfy

--- dist/app.js
+++ dist/app.js
@@ -10427,7 +10427,7 @@ return jQuery;
  * 
  *   typed.js - A JavaScript Typing Animation Library
  *   Author: Matt Boldt <me@mattboldt.com>
- *   Version: v2.0.8
+ *   Version: v2.0.9
  *   Url: https://github.com/mattboldt/typed.js
  *   License(s): MIT
  * 
